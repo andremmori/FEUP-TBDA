3fee764ddd6cc950ce4ee571ca008efca69d82a2
Andre <andremori16@gmail.com>
2021-04-29 16:36:02 +0100

    Added creation to project 2

---
+++ projects/project 2/create.sql
@@ -0,0 +1,112 @@
+DROP TABLE "ACTIVITIES" CASCADE CONSTRAINTS;
+DROP TABLE "DISTRICTS" CASCADE CONSTRAINTS;
+DROP TABLE "FACILITIES" CASCADE CONSTRAINTS;
+DROP TABLE "MUNICIPALITIES" CASCADE CONSTRAINTS;
+DROP TABLE "REGIONS" CASCADE CONSTRAINTS;
+DROP TABLE "ROOMTYPES" CASCADE CONSTRAINTS;
+DROP TABLE "USES" CASCADE CONSTRAINTS;
+DROP TABLE "ACTIVITY_FACILITIES" CASCADE CONSTRAINTS;
+DROP TABLE "DISTRICT_MUNICIPALITIES" CASCADE CONSTRAINTS;
+DROP TABLE "FACILITY_ACTIVITIES" CASCADE CONSTRAINTS;
+DROP TABLE "MUNICIPALITY_FACILITIES" CASCADE CONSTRAINTS;
+DROP TABLE "REGION_DISTRICTS" CASCADE CONSTRAINTS;
+DROP TABLE "REGION_MUNICIPALITIES" CASCADE CONSTRAINTS;
+
+----------------------------
+
+------- Type Creation ------
+
+----------------------------
+
+CREATE OR REPLACE TYPE region_t AS OBJECT (
+    cod NUMBER(4, 0),
+    designation	VARCHAR2(50 BYTE),
+    nut1 VARCHAR2(50 BYTE)
+);
+
+CREATE OR REPLACE TYPE district_t AS OBJECT
+(
+    cod	NUMBER(4,0),
+    designation	VARCHAR2(50 BYTE),
+    region REF region_t
+);
+
+CREATE OR REPLACE TYPE districtsref_tab_t AS TABLE OF REF district_t;
+
+CREATE OR REPLACE TYPE municipality_t AS OBJECT
+(
+    cod	NUMBER(4,0),
+    designation	VARCHAR2(50 BYTE),
+    district REF district_t,
+    region REF region_t
+);
+
+CREATE OR REPLACE TYPE municipalitiesref_tab_t AS TABLE OF REF municipality_t;
+
+CREATE OR REPLACE TYPE roomtype_t AS OBJECT
+(
+    roomtype NUMBER(4,0),
+    description VARCHAR2(50 BYTE)
+);
+
+CREATE OR REPLACE TYPE facility_t AS OBJECT
+(
+    id NUMBER(4,0),
+    name VARCHAR2(80),
+    capacity NUMBER(8,0),
+    roomtype REF roomtype_t,
+    address VARCHAR2(80),
+    municipality REF municipality_t
+);
+
+CREATE OR REPLACE TYPE facilitiesref_tab_t AS TABLE OF REF facility_t;
+
+CREATE OR REPLACE TYPE activity_t AS OBJECT
+(
+    ref VARCHAR2(20),
+    activity VARCHAR2(20)
+);
+
+CREATE OR REPLACE TYPE activitiesref_tab_t AS TABLE OF REF activity_t;
+
+ALTER TYPE region_t
+ADD ATTRIBUTE (districts districtsref_tab_t) CASCADE;
+
+ALTER TYPE region_t
+ADD ATTRIBUTE (municipalities municipalitiesref_tab_t) CASCADE;
+
+ALTER TYPE district_t
+ADD ATTRIBUTE (municipalities municipalitiesref_tab_t) CASCADE;
+
+ALTER TYPE municipality_t
+ADD ATTRIBUTE (facilities facilitiesref_tab_t) CASCADE;
+
+ALTER TYPE facility_t
+ADD ATTRIBUTE (activities activitiesref_tab_t) CASCADE;
+
+ALTER TYPE activity_t
+ADD ATTRIBUTE (facilities facilitiesref_tab_t) CASCADE;
+
+----------------------------
+
+------ Table Creation ------
+
+----------------------------
+
+CREATE TABLE regions OF region_t
+    NESTED TABLE districts STORE AS region_districts
+    NESTED TABLE municipalities STORE AS region_municipalities;
+
+CREATE TABLE districts OF district_t
+    NESTED TABLE municipalities STORE AS district_municipalities;
+
+CREATE TABLE municipalities OF municipality_t
+    NESTED TABLE facilities STORE AS municipality_facilities;
+
+CREATE TABLE roomtypes OF roomtype_t;
+
+CREATE TABLE facilities OF facility_t
+    NESTED TABLE activities STORE AS facility_activities;
+
+CREATE TABLE activities OF activity_t
+    NESTED TABLE facilities STORE AS activity_facilities;

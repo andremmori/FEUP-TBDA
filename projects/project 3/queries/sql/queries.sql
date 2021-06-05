945b8fa828fe1b269bcc6a1a2c8c9146b4fe3091
Andre <andremori16@gmail.com>
2021-06-05 18:39:06 +0100

    Removed useless query

--- projects/project 3/queries/sql/queries.sql
+++ projects/project 3/queries/sql/queries.sql
@@ -1,9 +1,9 @@
--- 1
+-- A
 SELECT id, name, description, activity
 FROM facilities NATURAL JOIN roomtypes NATURAL JOIN uses NATURAL JOIN activities
 WHERE description  LIKE '%touros%' AND activity = 'teatro';
 
--- 2
+-- B
 SELECT regions.designation, count(*)
 FROM facilities NATURAL JOIN roomtypes INNER JOIN municipalities
 ON municipalities.cod = facilities.municipality
@@ -11,8 +11,7 @@ INNER JOIN regions ON municipalities.region = regions.cod
 WHERE description  LIKE '%touros%'
 GROUP BY regions.designation;
 
--- 3
----- a)
+-- C
 SELECT count(*) FROM
 municipalities
 WHERE cod NOT IN
@@ -22,18 +21,7 @@ INNER JOIN
 municipalities ON municipalities.cod = facilities.municipality
 WHERE activity = 'cinema');
 
-
----- b)
-SELECT count(*)
-FROM municipalities LEFT OUTER JOIN (SELECT municipality as cod, activity
-FROM facilities NATURAL JOIN uses NATURAL JOIN activities
-INNER JOIN
-municipalities ON municipalities.cod = facilities.municipality
-WHERE activity = 'cinema') cinemas
-ON municipalities.cod = cinemas.cod
-WHERE activity IS NULL;
-
--- 4
+-- D
 SELECT q2.designation, q1.activity, q1.facilities
 FROM (SELECT activity, max(facilities) as facilities
 FROM (SELECT municipality, activity, count(*) as facilities
@@ -45,15 +33,7 @@ ON municipalities.cod = facilities.municipality
 GROUP BY designation, activity) q2
 ON q2.activity = q1.activity AND q2.facilities = q1.facilities;
 
--- 5
-
-SELECT municipalities.designation, facilities.name, roomtypes.description
-FROM facilities NATURAL JOIN roomtypes INNER JOIN municipalities On
-municipalities.cod = facilities.municipality INNER JOIN districts ON
-districts.cod = municipalities.district
-WHERE districts.designation = 'Porto' AND roomtypes.description  LIKE '%touros%';
-
--- 6
+-- E
 SELECT designation,cod FROM districts WHERE cod NOT IN
 (
 SELECT
@@ -63,7 +43,7 @@ LEFT OUTER JOIN facilities on facilities.municipality = municipalities.cod
 WHERE id is null
 );
 
--- 7
+-- F
 SELECT d.cod,d.designation, ROUND(AVG(f.capacity),2) AS Avg_Cap_Per_District
 FROM districts d, municipalities m, facilities f
 WHERE m.district = d.cod AND f.municipality = m.cod
